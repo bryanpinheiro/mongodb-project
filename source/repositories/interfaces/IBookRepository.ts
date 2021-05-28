2541f5b40ec0b5bf9c19aa0c9b1dcf1eb843bccc
Bryan Souza <bryansouza_01@outlook.com>
2021-05-28 06:08:10 -0300

    fix interface

--- source/repositories/interfaces/IBookRepository.ts
+++ source/repositories/interfaces/IBookRepository.ts
@@ -1,10 +1,10 @@
 import { IBook } from "@interfaces";
 
 export default interface IBookRepository {
-    createBook: (book: IBook) => Promise<IBook>;
-    getAllBooks: () => Promise<any>;
-    getBookById: () => Promise<IBook | null>;
-    updateBookById: (id: string, book: IBook) => Promise<IBook | null>;
-    updateBookExtraInformationById: (id: string, extraInformation: string) => Promise<IBook | null>;
-    deleteBookById: (id: string) => Promise<any>;
+    createBook: (book: IBook) => Promise<IBook>,
+    getAllBooks: () => Promise<any>,
+    getBookById: (id: string) => Promise<IBook | null>,
+    updateBookById: (id: string, book: IBook) => Promise<IBook | null>,
+    updateBookExtraInformationById: (id: string, extraInformation: string) => Promise<IBook | null>,
+    deleteBookById: (id: string) => Promise<any>
 }
